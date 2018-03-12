4d93a3cf55fd0fd4f427c1eec351c7451a1f5af3
chulwoopack <chulwoo.pack@gmail.com>
2018-03-12 11:39:00 -0500

    Final model

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ import model
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', '/work/cse496dl/cpack/Assignment_3/dataset/', 'directory where CIFAR-100 and CIFAR-10 is located')
 flags.DEFINE_string('save_dir', '/work/cse496dl/cpack/Assignment_3/models/1', 'directory where VAE model graph and weights are saved')
-flags.DEFINE_integer('batch_size', 500, '')
+flags.DEFINE_integer('batch_size', 250, '')
 flags.DEFINE_integer('latent_size', 32, '')
+flags.DEFINE_integer('max_epoch', 100, '')
 flags.DEFINE_integer('early_stop', 15, '')
 FLAGS = flags.FLAGS
 
@@ -36,13 +37,15 @@ test_data = cifar100_test[b'data']
 train_data = np.reshape(train_data,(50000, 3, 32, 32)).transpose(0,2,3,1).astype(float)
 test_data = np.reshape(test_data,(10000, 3, 32, 32)).transpose(0,2,3,1).astype(float)
 # Normalization: Input image should be normalized to have 0 mean and unit variation for the VAE.
-for i in range(50000):
+#for i in range(50000):
     #train_data[i] = train_data[i]-np.mean(train_data[i])
     #train_data[i] = train_data[i]/np.std(train_data[i])
-    train_data[i] = train_data[i]/255.0
+#    train_data[i] = train_data[i]/255.0
 
-for i in range(10000):
-    test_data[i] = test_data[i]/255.0
+#print(train_data[0])
+
+#for i in range(10000):
+#    test_data[i] = test_data[i]/255.0
 
 ################
 # VAE Modeling #
@@ -50,17 +53,39 @@ for i in range(10000):
 img_shape = [32, 32, 3]
 tf.reset_default_graph()
 inputs = tf.placeholder(tf.float32, shape=[None] + img_shape, name='encoder_input')
+inputs_norm = tf.div(
+   tf.subtract(
+      inputs, 
+      tf.reduce_min(inputs)
+   ), 
+   tf.subtract(
+      tf.reduce_max(inputs), 
+      tf.reduce_min(inputs)
+   )
+)
+drop_prob = tf.placeholder(dtype=tf.float32, shape=(), name='drop_prob')
 ## ENCODER
-means, log_scales = model.gaussian_encoder(inputs, FLAGS.latent_size)  # (?, 4, 4, 8)
+means, log_scales = model.gaussian_encoder(inputs_norm, FLAGS.latent_size, drop_prob)  # (?, 4, 4, 8)
 codes = model.gaussian_sample(means, log_scales)                 # (?, 4, 4, 8)
 tf.identity(codes,name='encoder_output')
 ## DECODER
-outputs = model.decoder(codes, name='decoder_output')
+outputs = model.decoder(codes, drop_prob)
+tf.identity(outputs,name='decoder_output')
+#outputs_norm = tf.div(
+#   tf.subtract(
+#      outputs, 
+#      tf.reduce_min(outputs)
+#   ), 
+#   tf.subtract(
+#      tf.reduce_max(outputs), 
+#      tf.reduce_min(outputs)
+#   )
+#)
 
 # calculate loss with learnable parameter for output log_scale
 with tf.name_scope('loss') as scope:
     output_log_scale = tf.get_variable("output_log_scale", initializer=tf.constant(0.0, shape=img_shape))
-    reconstruction_loss, latent_loss = util.vae_loss(inputs, outputs, means, log_scales, 'bernoulli', output_log_scale)
+    reconstruction_loss, latent_loss = util.vae_loss(inputs_norm, outputs, means, log_scales, 'bernoulli', output_log_scale)
     total_loss = reconstruction_loss + latent_loss
    
 ################
@@ -77,11 +102,11 @@ with tf.name_scope('optimizer') as scope:
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	for epoch in range(30):
+	for epoch in range(FLAGS.max_epoch):
 	    print("epoch... ", epoch)
 	    for i in range(train_data.shape[0] // FLAGS.batch_size):
 	        batch_xs = train_data[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
-	        _, gen_loss, lat_loss = sess.run([train_op, reconstruction_loss, latent_loss], {inputs: batch_xs})
+	        _, gen_loss, lat_loss = sess.run([train_op, reconstruction_loss, latent_loss], {inputs: batch_xs, drop_prob: 0.7})
 	        tot_loss = gen_loss + lat_loss
 	        # SAVE THE BEST MODEL
 	        if tot_loss < best_loss:
@@ -101,5 +126,14 @@ with tf.Session() as sess:
 	print("Done!")
 
 	# VISUALIZATION
-	#input_out, output_out = session.run([inputs, outputs], {inputs: my_test_data})
+	#idx = np.random.randint(test_data.shape[0])
+	#my_test_data = np.expand_dims(test_data[idx], axis=0)
+	
+	#input_out, output_log_scale_out, output_out = sess.run([inputs, output_log_scale, outputs], {inputs: my_test_data, drop_prob:1.0})
+	#output_out = (output_out-np.min(output_out))/(np.max(output_out)-np.min(output_out))
+	#fig = plt.figure()
+	#ax1 = fig.add_subplot(2,2,1)
+	#ax1.imshow(np.squeeze(input_out))
+	#ax2 = fig.add_subplot(2,2,2)
+	#ax2.imshow(np.squeeze(output_out))
 
--- model.py
+++ model.py
@@ -5,6 +5,9 @@ import numpy as np
 from tensorflow.python.framework import tensor_util
 import math
 
+def lrelu(x, alpha=0.3):
+    return tf.maximum(x, tf.multiply(x, alpha))
+
 def downscale_block(inputs, filters, downscale=1):
     """
     Args:
@@ -12,32 +15,36 @@ def downscale_block(inputs, filters, downscale=1):
         - filters: int
     """
     with tf.name_scope('conv_block') as scope:
-        conv = tf.layers.conv2d(inputs, filters, 2, 1, padding='same', activation=tf.nn.elu)
-        pool = tf.layers.max_pooling2d(conv, pool_size=(2,2), strides=downscale, padding='same')
-        return pool                              
+        conv = tf.layers.conv2d(inputs, filters, kernel_size=4, strides=2, padding='same', activation=lrelu)
+        #pool = tf.layers.max_pooling2d(conv, pool_size=(2,2), strides=downscale, padding='same')
+        return conv
 
 def upscale_block(x, scale=2):
     """ [Sub-Pixel Convolution](https://arxiv.org/abs/1609.05158) """
     n, w, h, c = x.get_shape().as_list()
-    x = tf.layers.conv2d(x, c * scale ** 2, (3, 3), activation=tf.nn.relu, padding='same')
+    x = tf.layers.conv2d(x, c * scale ** 2, kernel_size=(4, 4), activation=lrelu, padding='same')
     output = tf.depth_to_space(x, scale)
     return output
 
-def decoder(codes, name):
+def decoder(codes, drop_prob):
     tf.identity(codes,name='decoder_input')
     hidden_decoder = tf.layers.dense(codes,  4*4*3)     # (?,  4x4x8)
     decoder_4 = tf.reshape(hidden_decoder, [-1,4,4,3])  # (?,  4,  4, 16)
     decoder_8 = upscale_block(decoder_4)                # (?,  8,  8,  8)
+    decoder_8 = tf.nn.dropout(decoder_8, drop_prob)
     decoder_16 = upscale_block(decoder_8)               # (?, 16, 16,  4)
+    decoder_16 = tf.nn.dropout(decoder_16, drop_prob)
     output = upscale_block(decoder_16)                  # (?, 32, 32,  3)
     return output
 
-def gaussian_encoder(inputs, latent_size):
+def gaussian_encoder(inputs, latent_size, drop_prob):
     """inputs should be a tensor of images whose height and width are multiples of 4"""
     with tf.name_scope('downscale_1') as scope:
         conv_16 = downscale_block(inputs, 3, downscale=2)      # (?, 16, 16,  4)
+        conv_16 = tf.nn.dropout(conv_16, drop_prob)
     with tf.name_scope('downscale_2') as scope:
         conv_8 = downscale_block(conv_16, 3, downscale=2)      # (?,  8,  8,  8)
+        conv_8 = tf.nn.dropout(conv_8, drop_prob)
     with tf.name_scope('downscale_3') as scope:
         conv_4 = downscale_block(conv_8, 3, downscale=2)       # (?,  4,  4, 16)
     conv_4_flat = tf.reshape(conv_4, [-1, 4 * 4 * 3])
